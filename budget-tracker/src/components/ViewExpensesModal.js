17385a04b56240b947b8eaf427018b3d57250477
Sean Lightfoot <seanalexander360@gmail.com>
2022-01-31 02:47:51 -0600

    complete

---
+++ budget-tracker/src/components/ViewExpensesModal.js
@@ -0,0 +1,60 @@
+import { Button, Modal, Stack } from "react-bootstrap";
+import {
+  UNCATEGORIZED_BUDGET_ID,
+  useBudgets,
+} from "../contexts/BudgetsContext";
+import { currencyFormatter } from "../utils";
+
+export default function AddBudgetModal({ budgetId, handleClose }) {
+  const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } =
+    useBudgets();
+  const budget =
+    UNCATEGORIZED_BUDGET_ID === budgetId
+      ? {
+          name: "Uncategorized",
+          id: UNCATEGORIZED_BUDGET_ID,
+        }
+      : budgets.find((b) => b.id === budgetId);
+  const expenses = getBudgetExpenses(budgetId);
+  return (
+    <Modal show={budgetId != null} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <Stack direction="horizontal" gap="2">
+            <div>Expenses - {budget?.name}</div>
+            {budgetId !== UNCATEGORIZED_BUDGET_ID && (
+              <Button
+                variant="outline-danger"
+                onClick={() => {
+                  deleteBudget(budget.id);
+                  handleClose();
+                }}
+              >
+                Delete
+              </Button>
+            )}
+          </Stack>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Stack direction="vertical" gap={3}>
+          {expenses.map((expense) => (
+            <Stack key={expense.id} direction="horizontal" gap={2}>
+              <div className="me-auto fs-4">{expense.description}</div>
+              <div className="me-auto fs-5">
+                {currencyFormatter.format(expense.amount)}
+              </div>
+              <Button
+                size="sm"
+                variant="outline-danger"
+                onClick={() => deleteExpense(expense.id)}
+              >
+                &times;
+              </Button>
+            </Stack>
+          ))}
+        </Stack>
+      </Modal.Body>
+    </Modal>
+  );
+}
